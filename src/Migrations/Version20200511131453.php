4dffdc0276e3790b1d9849cecc8761daf2a9d552
Adrian Teletin <tele.bv@gmail.com>
2020-05-11 16:17:10 +0300

    sqlite update

--- src/Migrations/Version20200511131453.php
+++ src/Migrations/Version20200511131453.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200511115350 extends AbstractMigration
+final class Version20200511131453 extends AbstractMigration
 {
     public function getDescription() : string
     {
